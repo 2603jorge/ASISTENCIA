ec4a9c4c03a14af981eaa2c487fe7dd3eabb5ee6
2603jorge <samyoacats07@gmail.com>
2026-02-25 11:11:15 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,11 @@
-const CACHE_NAME = 'agricactus-v86-cache';
-const assets = ['./', 'index.html', 'manifest.json'];
+const CACHE_NAME = 'agricactus-v86-v1';
+const assets = [
+  './',
+  'index.html',
+  'manifest.json'
+];
 
+// Instala el Service Worker y guarda los archivos en caché
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
@@ -9,6 +14,7 @@ self.addEventListener('install', event => {
   );
 });
 
+// Permite que la app funcione sin conexión buscando primero en el caché
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
