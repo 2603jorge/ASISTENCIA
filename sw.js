957a7e3a70f4a13412f0ff3faab7a0b399afc968
2603jorge <samyoacats07@gmail.com>
2026-02-25 11:08:27 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,18 @@
-const CACHE_NAME = 'agricactus-v47-cache';
-const ASSETS = [
-  './',
-  './index.html', // o como se llame tu archivo principal
-  './manifest.json',
-  'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
-  'https://unpkg.com/html5-qrcode'
-];
+const CACHE_NAME = 'agricactus-v86-cache';
+const assets = ['./', 'index.html', 'manifest.json'];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
